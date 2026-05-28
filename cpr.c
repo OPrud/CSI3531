0fb1a6d0c02d7db5a42c94d8c7ec05dddc5c83ae
Lili Rose T <145508020+LiliRT@users.noreply.github.com>
2026-05-28 13:44:34 -0400

    Devoir 1 fait

--- cpr.c
+++ cpr.c
@@ -2,7 +2,7 @@
 Fichier: cpr.c
 
 Nom et Numéro étudiant: Océane Prud'Homme (300272920) et Lili Rose Théoret (300342096)
-
+Groupe #1 sur Brightspace.
 
 Description: Ce programme contient le code pour la creation
              d'un processus enfant et y attacher un tuyau.
@@ -12,17 +12,21 @@ Description: Ce programme contient le code pour la creation
 Explication du processus zombie
 (point 5 de "A completer" dans le devoir):
 
-	(s.v.p. completez cette partie);
 	Un processus zombie est un processus enfant terminé dont le parent n’a pas encore récupéré le statut avec wait(). 
 	Il ne consomme pas de ressources, mais garde une entrée dans la table des processus. 
 	S’il y en a trop, ils peuvent empêcher la création de nouveaux processus
 	Lorsque le parent appelle wait(), le processus se termine normalement.
+	Dans ce cas-ci, le parent n'appelle jamais wait(). Le zombie "meurt" lorsque le parent termine.
 	Site reference: https://www.geeksforgeeks.org/operating-systems/zombie-processes-prevention/
 
 
 -------------------------------------------------------------*/
 #include <stdio.h>
 #include <sys/select.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 /* Prototype */
 void creerEnfantEtLire(int );
@@ -70,7 +74,76 @@ Description:
 
 void creerEnfantEtLire(int prcNum)
 {
+    int fd[2];	// Descripteurs de fichiers pour le bout de lecture et le bout d'écriture du tuyau
+    pid_t pid;	// PID
+    char buffer[256];	// Buffer pour les messages
+    int nbCharLus;	// Nombre de caractères lus via read()
 
-    /* S.V.P. completez cette fonction selon les
-       instructions du devoirs. */
+    sprintf(buffer, "Processus %d commence\n", prcNum);	// Print dans le tampon de mémoire
+    write(1, buffer, strlen(buffer));	// Écrire les données du tampon dans la sortie standard
+
+    fflush(stdout);	// Force l'écriture immédiate
+
+    if(prcNum == 1)	// Cas final du dernier enfant
+    {
+        sleep(5);	// Dormir pendant sqrt(25) secondes
+
+        sprintf(buffer, "Processus %d termine\n", prcNum);	// Print dans le tampon de mémoire
+        write(1, buffer, strlen(buffer));	// Écrire les données du tampon dans la sortie standard
+
+        sleep(10);	// Dormir pendant 5*2 secondes
+
+        return;	// Fin de la méthode
+    }
+
+    if(pipe(fd) == -1)	// Création du pipe
+    {
+        perror("pipe");
+        exit(1);
+    }
+
+    pid = fork();	// Création de l'enfant du processus parent
+
+    if(pid < 0)	// Erreur dans la création de l'enfant du processus parent
+    {
+        perror("fork");
+        exit(1);
+    }
+
+    if(pid == 0)	// Car l'enfant à 0 dans sa variable pid
+    {
+        char prochainNum[20];	// Pour indiquer le numéro du prochain enfant
+
+        close(fd[0]);	// Fermer le bout de lecture pour écrire
+
+        dup2(fd[1], 1);	// Redirige la sortie standard vers le bout d'écriture du pipe
+
+        close(fd[1]);	// Fermer le bout d'écriture puisque l'écriture est terminée
+
+        sprintf(prochainNum, "%d", prcNum - 1);	// Print dans le tampon de mémoire
+
+        char *args[] = {"./cpr", prochainNum, NULL};	// Arguments à passer à la prochaine exécution de cpr.c
+
+        execvp(args[0], args);	// Remplace l'image du processus avec le programme spécifié, ici cpr.c défini dans la ligne précédente
+
+        perror("execvp");	// En cas d'erreur du execvp, ne devrait pas s'exécuter
+        exit(1);	// Fin en cas d'erreur du execvp, ne devrait pas s'exécuter
+    }
+
+    else	// Si le pid est supérieur à 0, alors c'est le processus parent
+    {
+        close(fd[1]);	// Fermer le bout d'écriture pour pouvoir lire
+
+        while((nbCharLus = read(fd[0], buffer, sizeof(buffer))) > 0)	// Lecture du pipe
+        {
+            write(1, buffer, nbCharLus);	// Écriture dans le pipe
+        }
+
+        close(fd[0]);	// Fermer le bout de lecture
+
+        sprintf(buffer, "Processus %d termine\n", prcNum);	// Print dans le tampon de mémoire
+        write(1, buffer, strlen(buffer));	// Écrire les données du tampon dans la sortie standard
+
+        sleep(10);	// Dormir pendant 20/2 secondes
+    }
 }
